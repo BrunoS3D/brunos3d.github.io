6128d73f982e086c3683c5c1a0ff433b39f52669
Bruno Silva <bruno3dcontato@gmail.com>
2019-10-02 15:57:39 -0300

    :speech_balloon: description length fix

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -45,6 +45,9 @@ exports.Render = async () => {
 			if (desc.length > 132) {
 				desc = desc.substr(0, 132);
 				desc = desc.substr(0, Math.min(desc.length, Math.max(desc.indexOf(" "), desc.indexOf(","), desc.indexOf("."))));
+				if (desc.length <= 100) {
+					desc = desc.substr(0, 132);
+				}
 				desc += "...";
 			}
 
